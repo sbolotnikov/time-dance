47f1bb869a6fe2e73926616ceced1e8e83704d47
Sergey Bolotnikov <sbolotnikov@gmail.com>
2021-01-04 12:49:48 -0500

    Animation development

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,21 @@
 var heightW = window.innerHeight;
 var widthW = window.innerWidth;
 var moveXY=[{duration:4, x:Math.floor(widthW*.6), y:Math.floor(heightW*-0.1),ease:"linear"},
-{duration:2, x:Math.floor(widthW*.6),y:Math.floor(heightW*-0.5),ease:"linear"},
-{duration:4, x:Math.floor(widthW*.1),y:Math.floor(heightW*-0.5),ease: "back"},
-{duration:3, x:Math.floor(widthW*0.37),y:Math.floor(heightW*-0.27),ease: "slow"}];
+{duration:2, x:Math.floor(widthW*.6),y:Math.floor(heightW*-0.5),ease:"back"},
+{duration:4, x:Math.floor(widthW*.1),y:Math.floor(heightW*-0.5),ease: "linear"},
+{duration:3, x:Math.floor(widthW/2-(heightW*0.4*979/1518*.7)),y:-Math.floor((heightW-heightW*.4)/2),ease: "slow"}];
 
 var moveXY2=[{duration:4, x:Math.floor(widthW*-.5), y:Math.floor(heightW*0.1),ease:"linear"},
-{duration:2, x:Math.floor(widthW*-.5),y:Math.floor(heightW*0.5),ease:"linear"},
-{duration:4, x:Math.floor(0),y:Math.floor(heightW*0.5),ease: "back"},
-{duration:3, x:Math.floor(widthW*-0.38),y:Math.floor(heightW*0.31),ease: "slow"}];
+{duration:2, x:Math.floor(widthW*-.5),y:Math.floor(heightW*0.5),ease:"back"},
+{duration:4, x:Math.floor(0),y:Math.floor(heightW*0.5),ease: "linear"},
+{duration:3, x:-Math.floor(widthW/2-(heightW*0.4*1278/1537*.7)),y:Math.floor((heightW-heightW*(.4+.07))/2),ease: "slow"}];
 
 
 
 
   function startHtml() {
-  
+    document.querySelector("#couple").style.left=`${Math.floor((widthW-(heightW*0.4/1652*1534))/2+(heightW*0.4/1652*1534))}px`;
+    console.log(Math.floor((widthW-(heightW*0.4/1652*1534 ))/2))
     gsap.from("#dude", {duration:1,opacity:0});
     gsap.from("#girl", {duration:1,opacity:0});
     gsap.to("#leg1", {duration:1,transformOrigin:"5% -1%",rotate:20,delay:1});
@@ -89,7 +90,7 @@ var moveXY2=[{duration:4, x:Math.floor(widthW*-.5), y:Math.floor(heightW*0.1),ea
     gsap.timeline({repeat:11, delay:7})
     .to("#rightLegG", {duration:.5,transformOrigin:"right 0%",rotate:-30,ease: "back"})  
     .to("#rightLegG", {duration:.5,transformOrigin:"right 0%",rotate:0,ease: "back"})
-    gsap.timeline({repeat:10, delay:7})
+    gsap.timeline({repeat:11, delay:7})
     .to("#leftLegG", {duration:.5,transformOrigin:"5% -1%",rotate:30,ease: "back"})
     .to("#leftLegG", {duration:.5,transformOrigin:"5% -1%",rotate:0,ease: "back"})
 
@@ -104,8 +105,18 @@ var moveXY2=[{duration:4, x:Math.floor(widthW*-.5), y:Math.floor(heightW*0.1),ea
     .to("#leg2", {duration:.5,transformOrigin:"1% 0%",rotate:0,ease: "back"})
 
 
-    gsap.timeline({repeat:10, delay:6.4})
+    gsap.timeline({repeat:11, delay:6.4})
     .to("#leg1", {duration:.5,transformOrigin:"5% -1%",rotate:30,ease: "back"})
     .to("#leg1", {duration:.5,transformOrigin:"5% -1%",rotate:0,ease: "back"})
 
+    gsap.to("#dude",{delay:20, opacity:0,duration:1});
+    gsap.to("#girl",{delay:20, opacity:0,duration:1});
+
+    gsap.timeline({delay:20})
+    .to("#couple",{opacity:1,duration:1})
+    .to("#couple",{duration:2,rotationY:360,transformOrigin:"-25vw 50%"})
+    .to("#couple",{x:-Math.floor((widthW-(heightW*0.4/1534*1652))/2), y:-Math.floor((heightW*.3)), duration:2})
+    .to("#couple",{scale:.3,transformOrigin:`${Math.floor((widthW-375)/905*47)-75}vw ${5}vh`, duration:1})
+    // -28vw 5vh
+console.log((widthW-375)/905*47-75)
   }
